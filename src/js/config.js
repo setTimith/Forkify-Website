2e9d270db82ebae87a8f6a8057fcd795385e8639
thomasabboud2 <tabboud997@gmail.com>
2022-03-13 14:34:44 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4d18c8c5-9099-4406-b2e0-9b6f59e25124';
+export const MODAL_CLOSE_SEC = 2.5;
+
+console.log(dsds);
